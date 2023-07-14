881e834f38a2b17694a7e39be2f13ea94d6dc069
Rafael de Alvarenga Reis <rafaareis.ebac@gmail.com>
2023-07-14 15:09:35 -0300

    Commit 14072023-1508

---
+++ transacao.py
@@ -0,0 +1,10 @@
+dia_11_saldo_inicial = 1000
+
+dia_11_transacao_1 = 243
+dia_11_transacao_2 = -798.58
+dia_11_transacao_3 = 427.12
+dia_11_transacao_4 = -10.91
+
+dia_11_saldo_final = dia_11_saldo_inicial + dia_11_transacao_1 + dia_11_transacao_2 + dia_11_transacao_3 + dia_11_transacao_4
+
+print(dia_11_saldo_final)
